6a8651924f2f68ac5aec90fd952dae11b3f6845f
Sara Schoonover <Saramschoon@gmail.com>
2023-09-29 18:06:27 +0000

    completed actors details page

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -7,7 +7,10 @@ class ActorsController < ApplicationController
 
   def actor_details
     @actor = Actor.find(params[:id])
+    @personal_info = Actor.where({:id => @actor}).at(0)
 
+    @characters = Character.where({:actor_id => @actor.id})
+    
     render({ :template => "actor_template/actor_details"})
   end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,15 @@
 class MoviesController < ApplicationController
   def all_movies
     @movies = Movie.all
-
+    
     render({ :template => "movie_template/movie"})
   end
 
   def movie_details
     @movie = Movie.find(params[:id])
     @director = Director.where(id: @movie.director_id).first
+    
     render({ :template => "movie_template/movie_details"})
+    
   end
 end
